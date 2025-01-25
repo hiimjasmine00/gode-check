a0ee734e60443a1eb6db0bddf1bec619873da408
hiimjustin000 <52604018+hiimjustin000@users.noreply.github.com>
2025-01-25 12:57:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# gode-check
+A CLI that checks the hashes of Geode mods, comparing GitHub releases with workflow artifacts.\
+(Originally written in JavaScript by [Prevter](https://github.com/Prevter), rewritten in Rust by [hiimjasmine00](https://github.com/hiimjasmine00).)
+
+## Usage
+```bash
+gode-check <release link>
+```
+
+## License
+This project is licensed under the [MIT License](./LICENSE).
---
+++ src/main.rs
@@ -0,0 +1,257 @@
+use colored::Colorize;
+use serde_json::Value;
+
+use std::env;
+use std::fs;
+use std::io::Cursor;
+use std::path::PathBuf;
+use std::process;
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() < 2 {
+        eprintln!("{}", "Please provide a github release link".red());
+        process::exit(1);
+    }
+    let url = &args[1];
+    let parts = url.split('/').collect::<Vec<&str>>();
+    if parts.len() < 8 {
+        eprintln!("Invalid URL: {url}");
+        process::exit(1);
+    }
+
+    let repo = format!("{}/{}", parts[3], parts[4]);
+    let api_url = format!("https://api.github.com/repos/{repo}");
+    let tag = parts[7];
+
+    let client = reqwest::blocking::Client::new();
+
+    let release = client
+        .get(&format!("{api_url}/releases/tags/{tag}"))
+        .header("Accept", "application/json")
+        .header("User-Agent", "gode-check")
+        .send()
+        .unwrap_or_else(|e| {
+            eprintln!("{}", format!("Error fetching release: {:?}", e).red());
+            process::exit(1);
+        })
+        .json::<Value>()
+        .unwrap_or_else(|e| {
+            eprintln!("{}", format!("Error parsing release JSON: {:?}", e).red());
+            process::exit(1);
+        });
+
+    let mut release_commit: String = release["target_commitish"].as_str().unwrap_or_default().into();
+    if release_commit.len() != 40 {
+        release_commit = client
+            .get(&format!("{api_url}/tags"))
+            .header("Accept", "application/json")
+            .header("User-Agent", "gode-check")
+            .send()
+            .unwrap_or_else(|e| {
+                eprintln!("{}", format!("Error fetching tags: {:?}", e).red());
+                process::exit(1);
+            })
+            .json::<Value>()
+            .unwrap_or_else(|e| {
+                eprintln!("{}", format!("Error parsing tags JSON: {:?}", e).red());
+                process::exit(1);
+            })
+            .as_array()
+            .unwrap_or(&vec![])
+            .iter()
+            .find(|t| {
+                t["name"].as_str().unwrap_or_default() == tag
+            }).unwrap_or_else(|| {
+                eprintln!("{}", "No matching tag found".red());
+                process::exit(1);
+            })["commit"]["sha"].as_str().unwrap_or_default().into();
+    }
+
+    println!("Release found for commit: {}", release_commit.cyan());
+
+    let artifacts = client
+        .get(&format!("{api_url}/actions/artifacts"))
+        .header("Accept", "application/json")
+        .header("User-Agent", "gode-check")
+        .send()
+        .unwrap_or_else(|e| {
+            eprintln!("{}", format!("Error fetching artifacts: {:?}", e).red());
+            process::exit(1);
+        })
+        .json::<Value>()
+        .unwrap_or_else(|e| {
+            eprintln!("{}", format!("Error parsing artifacts JSON: {:?}", e).red());
+            process::exit(1);
+        })["artifacts"]
+        .as_array()
+        .unwrap_or(&vec![])
+        .iter()
+        .filter(|a| {
+            a["workflow_run"]["head_sha"].as_str().unwrap_or_default() == release_commit
+        })
+        .cloned()
+        .collect::<Vec<Value>>();
+
+    let artifacts_len = artifacts.len();
+    if artifacts_len < 1 {
+        eprintln!("{}", "No artifacts found for the commit".red());
+        process::exit(1);
+    } else if artifacts_len > 1 {
+        println!("{}", format!("{} artifacts for commit found!", artifacts_len).green());
+    } else {
+        println!("{}", "Artifact for commit found!".green());
+    }
+    
+    let temp_dir = std::env::temp_dir().join("gode-check");
+    if temp_dir.exists() {
+        fs::remove_dir_all(&temp_dir).unwrap_or_else(|e| {
+            eprintln!("{}", format!("Failed to clean temporary directory: {:?}", e).red());
+            process::exit(1);
+        });
+    }
+
+    fs::create_dir(&temp_dir).unwrap_or_else(|e| {
+        eprintln!("{}", format!("Failed to create temporary directory: {:?}", e).red());
+        process::exit(1);
+    });
+
+    let artifact_dir = temp_dir.join("artifact");
+    fs::create_dir(&artifact_dir).unwrap_or_else(|e| {
+        eprintln!("{}", format!("Failed to create artifact directory: {:?}", e).red());
+        process::exit(1);
+    });
+
+    let release_dir = temp_dir.join("release");
+    fs::create_dir(&release_dir).unwrap_or_else(|e| {
+        eprintln!("{}", format!("Failed to create release directory: {:?}", e).red());
+        process::exit(1);
+    });
+
+    let mut geode_files: Vec<Vec<PathBuf>> = vec![];
+
+    for i in 0..artifacts_len {
+        let artifact = &artifacts[i];
+        let id = artifact["id"].as_u64().unwrap_or_default();
+        let workflow_run_id = artifact["workflow_run"]["id"].as_u64().unwrap_or_default();
+        
+        if artifacts_len == 1 {
+            println!("Downloading artifact...");
+        } else {
+            println!("Downloading artifact {}...", i + 1);
+        }
+
+        let artifact_path = artifact_dir.join(i.to_string());
+        if !artifact_path.exists() {
+            fs::create_dir(&artifact_path).unwrap_or_else(|e| {
+                eprintln!("{}", format!("Failed to create artifact directory: {:?}", e).red());
+                process::exit(1);
+            });
+        }
+
+        let suite_id = client
+            .get(&format!("{api_url}/actions/runs/{workflow_run_id}"))
+            .header("Accept", "application/json")
+            .header("User-Agent", "gode-check")
+            .send()
+            .unwrap_or_else(|e| {
+                eprintln!("{}", format!("Error fetching workflow run: {:?}", e).red());
+                process::exit(1);
+            })
+            .json::<Value>()
+            .unwrap_or_else(|e| {
+                eprintln!("{}", format!("Error parsing workflow run JSON: {:?}", e).red());
+                process::exit(1);
+            })["check_suite"]["id"].as_u64().unwrap_or_default();
+
+        let mut zip_data: Cursor<Vec<u8>> = Cursor::new(vec![]);
+
+        client
+            .get(&format!("https://nightly.link/{repo}/suites/{suite_id}/artifacts/{id}"))
+            .header("Accept", "application/octet-stream")
+            .send()
+            .unwrap_or_else(|e| {
+                eprintln!("{}", format!("Error downloading artifact: {:?}", e).red());
+                process::exit(1);
+            })
+            .copy_to(&mut zip_data)
+            .unwrap_or_else(|e| {
+                eprintln!("{}", format!("Error reading artifact file: {:?}", e).red());
+                process::exit(1);
+            });
+
+        zip_extract::extract(zip_data, &artifact_path, false).unwrap_or_else(|e| {
+            eprintln!("{}", format!("Error extracting artifact: {:?}", e).red());
+            process::exit(1);
+        });
+
+        geode_files.push(fs::read_dir(&artifact_path).unwrap_or_else(|e| {
+            eprintln!("{}", format!("Error reading artifact directory: {:?}", e).red());
+            process::exit(1);
+        }).filter_map(Result::ok).filter(|f| f.path().extension().map(|e| e == "geode").unwrap_or(false)).map(|f| f.path()).collect());
+    }
+
+    println!("Downloading release file...");
+
+    let release_asset = release["assets"].as_array().unwrap_or(&vec![]).iter().find(|asset| {
+        asset["name"].as_str().unwrap_or_default().ends_with(".geode")
+    }).unwrap_or_else(|| {
+        eprintln!("{}", "No .geode file found in the release".red());
+        process::exit(1);
+    }).clone();
+
+    let release_path = release_dir.join(release_asset["name"].as_str().unwrap_or_default());
+    client
+        .get(release_asset["browser_download_url"].as_str().unwrap_or_default())
+        .header("Accept", "application/octet-stream")
+        .send()
+        .unwrap_or_else(|e| {
+            eprintln!("{}", format!("Error downloading release file: {:?}", e).red());
+            process::exit(1);
+        })
+        .copy_to(&mut fs::File::create(&release_path).unwrap_or_else(|e| {
+            eprintln!("{}", format!("Error creating release file: {:?}", e).red());
+            process::exit(1);
+        }))
+        .unwrap_or_else(|e| {
+            eprintln!("{}", format!("Error saving release file: {:?}", e).red());
+            process::exit(1);
+        });
+
+    for i in 0..artifacts_len {
+        if geode_files.len() > 1 {
+            println!("{}", format!("Artifact {} ({}):", i + 1, &artifacts[i]["name"].as_str().unwrap_or_default()).yellow());
+        }
+        let artifact_files = &geode_files[i];
+        for artifact_file in artifact_files {
+            let artifact_data = fs::read(artifact_file).unwrap_or_else(|e| {
+                eprintln!("{}", format!("Error opening artifact file: {:?}", e).red());
+                process::exit(1);
+            });
+            let release_data = fs::read(&release_path).unwrap_or_else(|e| {
+                eprintln!("{}", format!("Error opening release file: {:?}", e).red());
+                process::exit(1);
+            });
+
+            let artifact_hash = sha256::digest(artifact_data);
+            let release_hash = sha256::digest(release_data);
+
+            if artifact_hash == release_hash {
+                println!("{}", format!("{}: {}", if artifact_files.len() > 1 {
+                    format!("{} Comparison", artifact_file.file_name().unwrap_or_default().to_string_lossy()).blue()
+                } else {
+                    "Comparison".blue()
+                }, "✅ Match".green()));
+            } else {
+                println!("{}", format!("{}: {}", if artifact_files.len() > 1 {
+                    format!("{} Comparison", artifact_file.file_name().unwrap_or_default().to_string_lossy()).blue()
+                } else {
+                    "Comparison".blue()
+                }, "❌ Mismatch".red()));
+            }
+
+            println!("Artifact hash: {}", artifact_hash.cyan());
+            println!("Release hash: {}", release_hash.cyan());
+        }
+    }
+}
